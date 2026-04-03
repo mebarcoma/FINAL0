8d55b197265b35bbb1ecfeaa6eeb572a862c7a29
mebarcoma <barcomamarjhon@gmail.com>
2026-04-03 12:35:18 +0800

    commit

---
+++ TIPCALCULATOR/app/src/main/java/com/example/tipcalculator/TipCalculator.java
@@ -0,0 +1,9 @@
+package com.example.tipcalculator;
+
+
+public class TipCalculator {
+    // Method to calculate tip
+    public double calculateTip(double tipPercent, double amount) {
+        return amount * tipPercent;
+    }
+}
